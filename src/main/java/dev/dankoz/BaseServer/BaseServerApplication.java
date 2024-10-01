1e8e968d0e80c18fa51d6bebb08474831ab687f8
Daannko <daniel.kozarski@gmail.com>
2024-10-01 17:08:32 +0200

    Re-initialized, added docker support.

---
+++ src/main/java/dev/dankoz/BaseServer/BaseServerApplication.java
@@ -0,0 +1,13 @@
+package dev.dankoz.BaseServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseServerApplication.class, args);
+	}
+
+}
